24b9b31719eff30edb2b443f81dd137b91ceb98c
Phan Anh <phananh12042003@gmail.com>
2023-07-02 02:28:50 -0400

    Dark Mode 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import HeroSection from "./components/HeroSection";
 
 function App() {
+  const [theme, setTheme] = useState("");
+
+  useEffect(function () {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
+  const handleThemeSwitcher = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
   return (
-    <div className="font-inter">
-      <HeroSection></HeroSection>
-    </div>
+    <>
+      <button
+        type="button"
+        onClick={handleThemeSwitcher}
+        className="fixed right-2 top-2 z-10 rounded-md bg-indigo-500 p-1 text-lg"
+      >
+        {theme === "dark" ? "🌙  " : "☀️"}
+      </button>
+      <div className="bg:white font-inter dark:bg-slate-900">
+        <HeroSection></HeroSection>
+      </div>
+    </>
   );
 }
 
 export default App;
-// 🚀
+// 🚀 👋🌙
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,10 +2,10 @@ function HeroSection() {
   return (
     <div className="flex flex-col items-center justify-center py-20">
       <div className="text-center">
-        <h1 className="mb-1 font-inter text-2xl font-semibold text-indigo-600 md:mb-3 md:text-4xl">
+        <h1 className="mb-1 font-inter text-2xl font-semibold text-indigo-600 dark:text-indigo-500 md:mb-3 md:text-4xl">
           Hi, This is Phan
         </h1>
-        <p className="text-md mb-3 max-w-md text-gray-600 md:text-xl">
+        <p className="text-md mb-3 max-w-md text-gray-600 dark:text-gray-300 md:text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Non corrupti
         </p>
         <a
